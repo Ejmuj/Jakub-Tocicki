6d97a73376b361c210768a9c4eb65ee8dbc7d215
javedkubbe <ejmuj1@gmail.com>
2024-11-27 15:40:13 +0100

    Stor bokstav

--- Javascript/Stein-saks-papir.js
+++ Javascript/Stein-saks-papir.js
@@ -5,16 +5,16 @@ document.getElementById("play").addEventListener("click", play) //Her får jeg t
  
 function play() {
     let value = document.getElementById("choices").value //gir stein saks eller papir en verdi slik att alle tre har en annerledes verdi.
-    const choices = ["stein", "papir", "saks"] //her er stein saks papir elementene
+    const choices = ["Stein", "Papir", "Saks"] //her er stein saks papir elementene
     let robot = choices[Math.floor(Math.random() * 3)] //får roboten til å velge stein saks eller papir tillfeldig
     if (!choices.includes(value)) {
         console.log("Not valid choice") // fielmelding om man ikke ha valgt noe akseptabelt
     }
     if (value === robot) {
         document.getElementById("result").innerText = "It's a tie";
-    } else if ((value === "stein" && robot === "saks") ||
-               (value === "papir" && robot === "stein") || // denne koden skriver ut hva som skjer om en av disse 3 scenarioene skjer
-               (value === "saks" && robot === "papir")) {  
+    } else if ((value === "stein" && robot === "Saks") ||
+               (value === "papir" && robot === "Stein") || // denne koden skriver ut hva som skjer om en av disse 3 scenarioene skjer
+               (value === "saks" && robot === "Papir")) {  
         document.getElementById("result").innerText = "Player won";
         humanScore++; // øker humanScore slik at jeg kan skrive ut senere hvor mye poeng player har
     } else {
