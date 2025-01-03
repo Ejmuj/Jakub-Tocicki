7e07bd3902b846791834be090fbd353fef061245
javedkubbe <ejmuj1@gmail.com>
2025-01-03 08:35:14 +0100

    s

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,6 +1,6 @@
 function showPrompt() {
     document.getElementById('overlay').style.display = 'block';
-    document.getElementById('promptBox').style.display = 'block'; // viser boxen hvor du skriver navnet ditt i
+    document.getElementById('promptBox').style.display = 'block'; // viser boksen hvor du skriver navnet ditt i
 } 
 
 
@@ -9,8 +9,8 @@ function submitPrompt() {
     document.getElementById('result').innerText = `Hei, ${userInput}!`;
     
 
-    document.getElementById('overlay').style.display = 'none';
-    document.getElementById('promptBox').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none'; 
+    document.getElementById('promptBox').style.display = 'none'; // Dette er Knappen du kan trykke på for å levere svaret
     
 
     document.getElementById('userInput').value = '';
